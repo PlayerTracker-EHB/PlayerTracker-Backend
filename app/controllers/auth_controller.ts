d8c6b96a99902bdca519a87e06f9d6df6db8b4c2
Mehdi Merkachi <merkachimehdi@gmail.com>
2025-02-08 20:19:46 +0100

    Feature: Basic session auth

---
+++ app/controllers/auth_controller.ts
@@ -0,0 +1,38 @@
+import type { HttpContext } from '@adonisjs/core/http'
+
+import User from '#models/user'
+
+export default class AuthController {
+  public async register({ request, response, auth }: HttpContext) {
+    const data = request.only(['email', 'password'])
+
+    const user = await User.create(data)
+    await auth.use('web').login(user)
+
+    return response.ok({ user, message: 'User registered successfully' })
+  }
+
+  public async login({ request, response, auth }: HttpContext) {
+    try {
+      const { email, password } = request.only(['email', 'password'])
+
+      const user = await User.verifyCredentials(email, password)
+
+      await auth.use('web').login(user)
+
+      return response.ok({ user, message: 'Login successful' })
+    } catch (error) {
+      console.error('login-error:', error)
+      return response.unauthorized({ message: 'Invalid credentials' })
+    }
+  }
+
+  public async logout({ auth, response }: HttpContext) {
+    await auth.use('web').logout()
+    return response.ok({ message: 'Logged out successfully' })
+  }
+
+  public async me({ auth, response }: HttpContext) {
+    return response.ok({ user: auth.user })
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -6,11 +6,18 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
+const AuthController = () => import('#controllers/auth_controller')
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+
+router.post('/register', [AuthController, 'register'])
+router.post('/login', [AuthController, 'login'])
+router.post('/logout', [AuthController, 'logout'])
+router.get('/me', [AuthController, 'me']).use(middleware.auth())
