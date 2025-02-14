5d3f396b274dbbf895039de5b2a36420670f002b
Mehdi Merkachi <merkachimehdi@gmail.com>
2025-02-14 23:10:01 +0100

    Added backend file validation

--- app/controllers/UploadController.ts
+++ app/controllers/UploadController.ts
@@ -2,30 +2,39 @@ import { HttpContext } from '@adonisjs/core/http'
 import { UploadService } from '#services/upload_service'
 import { inject } from '@adonisjs/core'
 
+
 @inject()
 export default class UploadsController {
   constructor(protected uploadService: UploadService) { }
 
   async uploadChunk({ request, response }: HttpContext) {
-    const chunk = request.file('chunk')
-    console.log("uploadChunk called")
-    console.log("Received chunk:", chunk)
-
-    const { name, chunkIndex } = request.body()
-    console.log("Chunk details - Name:", name, "Index:", chunkIndex)
+    const chunk = request.file('chunk', {
+      size: '1gb',
+      extnames: ['mp4', 'mkv', 'avi', 'mov', 'wmv']
+    })
 
     if (!chunk) {
-      console.error("No chunk provided")
       return response.badRequest({ error: 'No chunk provided' })
     }
 
+
+    const { name, chunkIndex } = request.body()
+
+    // Only validate first chunk
+    if (Number(chunkIndex) === 0) {
+      if (!chunk?.isValid) {
+        console.error("Invalid file type:", chunk?.errors)
+        return response.badRequest({
+          error: 'Invalid file type',
+          details: chunk?.errors
+        })
+      }
+    }
+
     try {
-      console.log("Starting chunk upload for:", name, "Index:", chunkIndex)
       await this.uploadService.handleChunkUpload(chunk, name, Number(chunkIndex))
-      console.log("Chunk uploaded successfully for:", name, "Index:", chunkIndex)
       return response.ok({ message: 'Chunk uploaded successfully' })
     } catch (error) {
-      console.error("Error during chunk upload:", error)
       return response.internalServerError({
         error: 'Failed to upload chunk',
         details: error.message
@@ -35,16 +44,11 @@ export default class UploadsController {
 
   async finalizeUpload({ request, response }: HttpContext) {
     const { name, totalChunks } = request.body()
-    console.log("finalizeUpload called")
     console.log("Finalizing upload for:", name, "Total Chunks:", totalChunks)
-
     try {
-      console.log("Combining and storing file for:", name)
       await this.uploadService.combineAndStoreFile(name, parseInt(totalChunks))
-      console.log("Upload finalized successfully for:", name)
       return response.ok({ message: 'Upload finalized successfully' })
     } catch (error) {
-      console.error("Error during finalizing upload:", error)
       return response.internalServerError({
         error: 'Failed to finalize upload',
         details: error.message
